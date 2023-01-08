0b59ed21597bcf66e05b1bb21ab8cda3d2dbfd01
Anil Rana <102950907+anilkrrana@users.noreply.github.com>
2023-01-08 23:22:25 +0530

    Update script.js

--- Random-choice-picker/script.js
+++ Random-choice-picker/script.js
@@ -29,7 +29,7 @@ function createTags(input) {
 }
 
 function randomSelect() {
-    const times = 30
+    const times = 30;
 
     const interval = setInterval(() => {
         const randomTag = pickRandomTag()
